9eec3759b587da67f8d594d099096008845465d6
Erik J. Olson <hello@erikjolson.com>
2018-11-06 12:18:45 -0600

    Update index.js

--- dc-metro-map/routes/index.js
+++ dc-metro-map/routes/index.js
@@ -61,7 +61,7 @@ function wmataJsonToGeoJson(jsonData) {
 //-----------------------------------------------------------------------------
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('dcmetro', { title: 'AC Metro Stations', BEERME: BEERME });
+  res.render('dcmetro', { title: 'Rolling Stones Metro Stations', BEERME: BEERME });
 });
 
 //-----------------------------------------------------------------------------
